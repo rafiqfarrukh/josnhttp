8532d59af769003869f765613dd96a1f02064162
frafiq <rafiq.farrukh@gmail.com>
2017-05-08 14:14:54 -0400

    josnhttp

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,159 +4,6 @@ import com.google.gson.Gson;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-//import java.util.*;
-//class Forecast {
-//    private String name;
-//    private List<Double> forecast;
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public List<Double> getForecast() {
-//        return new ArrayList<>(forecast);
-//    }
-//
-//    public void setForecast(List<Double> forecast) {
-//        this.forecast = forecast;
-//    }
-//
-//    public String toString() {
-//        List<String> forecastStrings = new ArrayList<>();
-//        for (Double temp: forecast) {
-//            forecastStrings.add(temp.toString());
-//        }
-//        String forecastString = String.join(", ", forecastStrings);
-//        return String.format("The forecast for %s: is %s", name, forecastString);
-//    }
-//}
-//
-//class ForecastCollection extends ArrayList<Forecast> {}
-//
-//
-//public class Main {
-//    static final String FILENAME = "data.json";
-//
-//    public static ForecastCollection load(String filename) throws IOException {
-//        Gson gson = new Gson();
-//        FileReader reader = new FileReader(filename);
-//        try {
-//            return gson.fromJson(reader, ForecastCollection.class);
-//        }
-//        finally {
-//            reader.close();
-//        }
-//    }
-//
-//
-//    public static void save(String filename, ForecastCollection data) throws IOException {
-//        Gson gson = new Gson();
-//        FileWriter writer = new FileWriter(filename);
-//        try {
-//            gson.toJson(data, writer);
-//        }
-//        finally {
-//            writer.close();
-//        }
-//    }
-//
-//
-//
-//
-//
-//
-//
-//    public static void main(String[] args) {
-//        try {
-//
-//            // load forecasts from file
-//            ForecastCollection forecasts = load(FILENAME);
-//
-//            // print each forecast from the file
-//            for (Forecast f: forecasts) {
-//                System.out.println(f);
-//            }
-//
-//            // create new forecast
-//            Forecast dayton = new Forecast();
-//            dayton.setName("Dayton");
-//
-//            List<Double> daytonForecast = new ArrayList<>();
-//            daytonForecast.add(70.0);
-//            daytonForecast.add(75.0);
-//            daytonForecast.add(80.0);
-//            daytonForecast.add(72.5);
-//            daytonForecast.add(70.0);
-//
-//            dayton.setForecast(daytonForecast);
-//
-//            forecasts.add(dayton);
-//
-//            // save the data to a file
-//            save(FILENAME, forecasts);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//}
-//class Task {
-//    private String body;
-//    private boolean done;
-//    private int id;
-//    private int priority;
-//    private String title;
-//
-//    public String toString() {
-//        return title + ": " + body;
-//    }
-//}
-//
-//class TaskCollection implements Iterable<Task>{
-//    // should be private; public for simplicity
-//    private List<Task> todos;
-//
-//    public Iterator<Task> iterator() {
-//        return todos.iterator();
-//    }
-//}
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        String jsonData = "{\n" +
-//                "  \"todos\": [\n" +
-//                "    {\n" +
-//                "      \"body\": \"Walk the dog\",\n" +
-//                "      \"done\": false,\n" +
-//                "      \"id\": 0,\n" +
-//                "      \"priority\": 3,\n" +
-//                "      \"title\": \"dog\"\n" +
-//                "    },\n" +
-//                "    {\n" +
-//                "      \"body\": \"Pay the bills\",\n" +
-//                "      \"done\": false,\n" +
-//                "      \"id\": 1,\n" +
-//                "      \"priority\": 1,\n" +
-//                "      \"title\": \"bills\"\n" +
-//                "    }\n" +
-//                "  ]\n" +
-//                "}";
-//
-//        Gson gson = new Gson();
-//        TaskCollection tasks = gson.fromJson(jsonData, TaskCollection.class);
-//        for (Task t: tasks) {
-//            System.out.println(t);
-//        }
-//
-//        System.out.println(gson.toJson(tasks));
-//
-//    }
-//}
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.ToIntFunction;
@@ -342,7 +189,9 @@ public class Main {
         }
         catch (IOException e) {
             e.printStackTrace();
+
         }
+
         choose = inputMethod();
         while(choose != 0){
             if(choose == 1){
@@ -363,8 +212,6 @@ public class Main {
                         isInt = true;
                         myTask.setPriority(inputPriority);
                         collectionOfTasks.add(myTask);
-                        // save the data to a file
-                        save(FILENAME, collectionOfTasks);
                     }
                     catch (Exception e) {
                         // while not int
@@ -382,8 +229,6 @@ public class Main {
                         indexLocation = Integer.parseInt(input.nextLine());
                         isInt = true;
                         collectionOfTasks.remove(indexLocation);
-                        // save the data to a file
-                        save(FILENAME, collectionOfTasks);
                     }
                     catch (Exception e){
                         System.out.println("Enter the index of the task to remove: ");
@@ -421,8 +266,6 @@ public class Main {
                         myTask.setPriority(Integer.parseInt(input.nextLine()));
                         isInt = true;
                         collectionOfTasks.update(indexLocation,myTask);
-                        // save the data to a file
-                        save(FILENAME, collectionOfTasks);
                     }
                     catch (Exception e) {
                         // while not int
@@ -461,7 +304,13 @@ public class Main {
             }
 
             choose=  inputMethod();
+            try { // save the data to a file
+                save(FILENAME, collectionOfTasks);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
 
+            } // save the data to a file
         }
     }
 }
